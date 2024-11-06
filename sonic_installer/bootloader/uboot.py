b40ae3af1f000bb35472e149aae180094522bcbc
centecqianj <qianj@centec.com>
2024-11-07 07:45:59 +0800

    zj support auto sync uboot-env between master and slaver spi flash

--- sonic_installer/bootloader/uboot.py
+++ sonic_installer/bootloader/uboot.py
@@ -6,6 +6,7 @@ import platform
 import subprocess
 
 import click
+import os
 
 from ..common import (
    HOST_PATH,
@@ -21,6 +22,38 @@ class UbootBootloader(OnieInstallerBootloader):
 
     SONIC_IMAGE_MAX = 2
 
+    def sonic_save_spi_env_to_emmc(self):
+        click.echo('Sonic del last spi-flash env in emmc.')
+        os.system("rm -rf /host/master_update_env_done")
+        os.system("rm -rf /host/slaver_update_env_done")
+        os.system("rm -rf /host/spi_flash_env_bck")
+        
+        proc = subprocess.Popen("hexdump -C /dev/mtd2 | grep 1220", shell=True, text=True, stdout=subprocess.PIPE)
+        (out, _) = proc.communicate()
+        if 'master' in out.rstrip():
+            ha_status = 'master'
+        elif 'slaver' in out.rstrip():
+            ha_status = 'slaver'
+        else:
+            click.echo('Please setv serialno 0x1220 master/slaver in uboot!')
+            return True
+            
+        os.system("mkdir /host/spi_flash_env_bck")
+        os.system("dd if=/dev/mtd1 of=/host/spi_flash_env_bck/mtd1_data")
+        os.system("dd if=/dev/mtd3 of=/host/spi_flash_env_bck/mtd3_data")
+       
+        if ha_status == 'master':
+            click.echo('Master save spi-flash env to emmc complete!')
+            os.system("mkdir /host/master_update_env_done")
+        else:
+            click.echo('Slaver save spi-flash env to emmc complete!')
+            os.system("mkdir /host/slaver_update_env_done")
+        
+        os.system("sync;sync;sync")
+        os.system("sleep 3")  # wait 3 seconds after sync
+        
+        return True
+        
     def get_installed_images(self):
         images = []
         for idx in range(1, self.SONIC_IMAGE_MAX + 1):
@@ -55,6 +88,7 @@ class UbootBootloader(OnieInstallerBootloader):
                 cmd = '/usr/bin/fw_setenv boot_next "run sonic_image_' + str(idx) + '"' + ' 2>/dev/null'
                 run_command(cmd)
                 break
+        self.sonic_save_spi_env_to_emmc()
         return True
 
     def set_next_image(self, image):
@@ -65,6 +99,7 @@ class UbootBootloader(OnieInstallerBootloader):
                 cmd = '/usr/bin/fw_setenv boot_once "run sonic_image_' + str(idx) + '"'
                 run_command(cmd)
                 break
+        self.sonic_save_spi_env_to_emmc()
         return True
 
     def install_image(self, image_path):
@@ -100,6 +135,7 @@ class UbootBootloader(OnieInstallerBootloader):
         run_command(cmd)
         click.echo('Removing image root filesystem...')
         subprocess.call(['rm','-rf', HOST_PATH + '/' + image_dir])
+        self.sonic_save_spi_env_to_emmc()
         click.echo('Done')
 
     @classmethod
