54b603a06cb2c098919829a0cba1e217a0be3855
Alan Yoush <yoush@centec.com>
2026-07-07 07:59:13 +0000

    [MODIFY:CENTEC] add exclude-vlan para for arp show without get fdb entry
    Signed-off-by: Alan Yoush <yoush@centec.com>

--- show/main.py
+++ show/main.py
@@ -421,8 +421,9 @@ def event_counters():
 @cli.command()
 @click.argument('ipaddress', required=False)
 @click.option('-if', '--iface')
+@click.option('--exclude-vlan', is_flag=True, help="Exclude vlan information")
 @click.option('--verbose', is_flag=True, help="Enable verbose output")
-def arp(ipaddress, iface, verbose):
+def arp(ipaddress, iface, exclude_vlan, verbose):
     """Show IP ARP table"""
     cmd = ['nbrshow', '-4']
 
@@ -437,6 +438,9 @@ def arp(ipaddress, iface, verbose):
 
         cmd += ['-if', str(iface)]
 
+    if exclude_vlan is not None:
+        cmd += ['--exclude-vlan']
+
     run_command(cmd, display_cmd=verbose)
 
 #
@@ -446,8 +450,9 @@ def arp(ipaddress, iface, verbose):
 @cli.command()
 @click.argument('ip6address', required=False)
 @click.option('-if', '--iface')
+@click.option('--exclude-vlan', is_flag=True, help="Exclude vlan information")
 @click.option('--verbose', is_flag=True, help="Enable verbose output")
-def ndp(ip6address, iface, verbose):
+def ndp(ip6address, iface, exclude_vlan, verbose):
     """Show IPv6 Neighbour table"""
     cmd = ['nbrshow', '-6']
 
@@ -456,6 +461,9 @@ def ndp(ip6address, iface, verbose):
 
     if iface is not None:
         cmd += ['-if', str(iface)]
+
+    if exclude_vlan is not None:
+        cmd += ['--exclude-vlan']
 
     run_command(cmd, display_cmd=verbose)
 
