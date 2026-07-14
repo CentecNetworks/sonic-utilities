1b976e57027720be6f0088ad504442d3db41925f
Alan Yoush <yoush@centec.com>
2026-07-14 12:35:36 +0000

    [MODIFY:YOUSH] merge PR #4621 improve the performance of show ip interface https://github.com/sonic-net/sonic-utilities/pull/4621
    Signed-off-by: Alan Yoush <yoush@centec.com>

--- tests/mock_tables/mock_multi_asic.py
+++ tests/mock_tables/mock_multi_asic.py
@@ -85,6 +85,56 @@ def mock_multi_asic_get_ip_intf_addr_from_ns(namespace, iface):
         pass
     return ipaddresses
 
+def _mock_netmask_to_prefixlen(netmask):
+    """
+    Convert a netmask string from mock_intf_table into a prefix length.
+    Accepts dotted-decimal IPv4 or 'mask/len' IPv6 forms.
+    """
+    if '/' in netmask:
+        return int(netmask.split('/', 1)[-1])
+    import netaddr
+    return netaddr.IPAddress(netmask).netmask_bits()
+
+
+def mock_multi_asic_get_kernel_intf_state(namespace, af):
+    """
+    Synthesize the (links, addrs) tuple that the production helper
+    ``multi_asic_get_kernel_intf_state`` returns by walking
+    ``mock_intf_table`` for the given namespace.
+    """
+    table = mock_intf_table.get(namespace, {})
+    links = []
+    addrs = []
+    for idx, iface in enumerate(table.keys(), start=1):
+        links.append({
+            'index': idx,
+            'name': iface,
+            # IFF_UP | IFF_BROADCAST | IFF_RUNNING — admin will be 'up'.
+            'flags': 0x1043,
+            'master_idx': None,
+            'operstate': 'UP',
+        })
+        for fam_key, entries in table[iface].items():
+            if fam_key != af:
+                continue
+            for entry in entries:
+                ip_str = entry.get('addr', '')
+                # Strip the %iface zone id from link-local IPv6 addresses
+                # in the mock data — production netlink never includes it
+                # and ipintutil re-appends it when rendering.
+                if '%' in ip_str:
+                    ip_str = ip_str.split('%', 1)[0]
+                netmask = entry.get('netmask', '')
+                try:
+                    prefixlen = _mock_netmask_to_prefixlen(netmask)
+                except (OSError, ValueError):
+                    continue
+                addrs.append({
+                    'index': idx,
+                    'addr': ip_str,
+                    'prefixlen': prefixlen,
+                })
+    return links, addrs
 
 multi_asic.get_num_asics = mock_get_num_asics
 multi_asic.is_multi_asic = mock_is_multi_asic
@@ -92,3 +142,4 @@ multi_asic.get_namespace_list = mock_get_namespace_list
 multi_asic.get_namespaces_from_linux = mock_get_namespace_list
 multi_asic_util.multi_asic_get_ip_intf_from_ns = mock_multi_asic_get_ip_intf_from_ns
 multi_asic_util.multi_asic_get_ip_intf_addr_from_ns = mock_multi_asic_get_ip_intf_addr_from_ns
+multi_asic_util.multi_asic_get_kernel_intf_state = mock_multi_asic_get_kernel_intf_state
